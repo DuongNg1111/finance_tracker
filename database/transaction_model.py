57238233b1ea49f6c4b7ecbfda46a519d7f90b2e
ledo-netizen <le.do.yb@renesas.com>
2025-11-28 10:33:53 +0700

    day7: continue on transaction CRUD

--- database/transaction_model.py
+++ database/transaction_model.py
@@ -4,7 +4,7 @@
 from database.database_manager import DatabaseManager
 import config
 from datetime import datetime, date
-from typing import Union
+from typing import Union, Any, Optional
 from bson.objectid import ObjectId
 
 collection_name = config.COLLECTIONS['transaction']
@@ -47,6 +47,58 @@ class TransactionModel:
                            transaction_id: Union[str, ObjectId]):
         result = self.collection.delete_one({"_id": ObjectId(transaction_id)})
         return result.deleted_count
+    
+    def get_transaction_by_id(self, transaction_id: Union[str, ObjectId]):
+        try:
+            result = self.collection.find_one({"_id": ObjectId(transaction_id)})
+            return result
+        except Exception as e:
+            print(f"Failed to retrieve transaction: {transaction_id}, error {e}")
+            return
 
-    def get_all_transactions(self):
-        pass
+    def get_all_transactions(self,
+                            filters: dict[str, Any] = None):
+        query_ = self._build_filter_query(filters)
+
+        cursor = self.collection.find(query_).sort("created_at", -1)
+        return list(cursor)
+
+    def _build_filter_query(self, filters: dict[str, Any]) -> dict:
+        query = {}
+
+        # Check transaction_type:
+        if "transaction_type" in filters:
+            query["type"] = filters.get("transaction_type")
+
+        # Check Category:
+        if "category" in filters:
+            query["category"] = filters.get("category")
+
+        # Check amount:
+        min_amount = filters.get("min_amount")
+        max_amount = filters.get("max_amount")
+        if min_amount or max_amount:
+            query['amount'] = {}
+            if min_amount is not None:
+                query['amount']["$gte"] = min_amount # $gte = greater than or equal
+            if max_amount is not None:
+                query['amount']["$lte"] = max_amount # $lte = less than or equal
+
+        # Check datetime
+        start_date = filters.get("start_date")
+        end_date = filters.get("end_date")
+        if start_date or end_date:
+            query['transaction_date'] = {}
+            if start_date is not None:
+                query['transaction_date']["$gte"] = start_date # $gte = greater than or equal
+            if end_date is not None:
+                query['transaction_date']["$lte"] = end_date # $lte = less than or equal
+
+        # Check description:
+        if "search_text" in filters:
+            query['description'] = {
+                "$regex": filters.get("search_text"),
+                "$option": "i"
+            }
+
+        return query
--- views/transaction_view.py
+++ views/transaction_view.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import config
 from datetime import date
+import time
 
 def _render_add_transaction(transaction_model, category_model):
     st.subheader("Add Transaction")
@@ -50,14 +51,87 @@ def _render_add_transaction(transaction_model, category_model):
         submitted = st.form_submit_button("Submit")
 
         if submitted:
-            st.balloons()
+            # st.balloons()
 
-            # TODO:
-            # call transaction_model.add_new_transaction(**args, **kwargs)
+            result = transaction_model.add_new_transaction(
+                transaction_type,
+                category,
+                amount,
+                transaction_date,
+                description
+            )
+            if result:
+                st.success(f"Transaction has been added !!!")
+                time.sleep(2) # Manually extend message display duration
+                st.rerun()
+            else:
+                st.error("Failed to add transaction")
+                time.sleep(1)
 
+def _render_transaction_detail(item, transaction_model):
+    transaction_date = item.get("transaction_date")
+    if not transaction_date:
+        transaction_date = "Unknown"
+    else:
+        transaction_date = transaction_date.strftime("%d/%m/%Y")
+
+    with st.expander(
+        f"{transaction_date} -"
+        f"{item.get("amount")} $"
+    ):
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write(f"***Transaction type: {item.get("type")}**")
+            st.write(f"***Category: {item.get("category")}**")
+            st.write(f"***Amount: {item.get("amount")} USD**")
+            st.caption(f"***Description: {item.get("description")}**")
+        with col2:
+            delete_button = st.button("❌", key = f"transaction_delete_{item["_id"]}")
+            if delete_button:
+                # TODO: call delete transaction
+                delete_result = transaction_model.delete_transaction(item["_id"])
+                if delete_result:
+                    st.success("Transaction delete")
+                    time.sleep(2)
+                    st.rerun()
+        with col3:
+            if st.button("✏️", key=f"transaction_edit_{item['_id']}"):
+                st.warning("Edit transaction")
+                time.sleep(2)
+
+                # TODO:
+                #. 1 Display modal contains transaction detail
+                # 1.1 trigger for show_edit_modal = True
+                # 2.2 .get_transaction_by_id
+                st.session_state['show_edit_modal'] = True
+                st.rerun()
+
+
+                # 2. change properties and clicked save
+                # call .update_transaction
+
+def _render_list_transaction(transaction_model):
+    st.subheader("List transaction")
+
+    results = transaction_model.get_all_transactions(filters = {})
+    if results:
+        # transactions = results['transactions']
+        for item in results:
+            _render_transaction_detail(item, transaction_model)
+
+@st.dialog("Edit Transaction")
+def _render_edit_modal(transaction_model):
+    st.title(f"Edit transaction section")
 
 def render_transactions(transaction_model, category_model):
     st.title("💳 Transaction Management")
+
+    # check if any edit button is clicked
+    if st.session_state.get("show_edit_modal", False):
+        _render_edit_modal(transaction_model)
+
+    # Display list of transactions:
+    _render_list_transaction(transaction_model)
 
     # Add transaction
     _render_add_transaction(transaction_model, category_model)
